bcf70219f977e6df701a732aef92845f67ee10ec
Jorge Antonio Ibarra <jorge.ibarra.ms@gmail.com>
2018-09-06 09:42:44 -0500

    cambio de logo

--- core/app/view/home-view.php
+++ core/app/view/home-view.php
@@ -9,7 +9,7 @@ foreach($events as $event){
 <div class="col-md-12">
 <div class="card" style="text-align: center;">
 
-<img class="img-responsive" src="assets/images/vmcomp.png" style="width: 80%; margin-left: 10%;">
+<img class="img-responsive" src="assets/images/logo.jpeg" style="width: 80%; margin-left: 10%;">
 </div>
 </div>
 </div>
