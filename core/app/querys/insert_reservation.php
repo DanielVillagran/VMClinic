8c4c5a85a0683b9964523520dc91f66f9b19bbe1
Oswaldo <ovillagrana@ant.com.mx>
2018-09-25 10:12:29 -0500

    Cambios probando el deploy automatico

--- core/app/querys/insert_reservation.php
+++ core/app/querys/insert_reservation.php
@@ -6,7 +6,7 @@ $reservation->title="puto";
 $reservation->date_at="perro";
 $reservation->time_at="08:00";
 $reservation->type="2";
-$reservation->created_at="NOW()";
+//$reservation->created_at="NOW()";
 $id=R::store($reservation);
 echo "<script>window.location='index.php?view=calendar';</script>";
 ?>
