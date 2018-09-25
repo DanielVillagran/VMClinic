073d263dec71fb503ac420b651b29bff3ff47cc8
Oswaldo <ovillagrana@ant.com.mx>
2018-09-25 08:57:24 -0500

    Cambios probando el deploy automatico

--- core/app/model/MuniData.php
+++ core/app/model/MuniData.php
@@ -9,8 +9,10 @@ class MuniData
     public function getMuni()
     {
     	require 'rb.php';
-    	R::setup( 'mysql:host=localhost;dbname=vmclinic',
-        'root', '' );
+    	R::setup( 'mysql:host=localhost;dbname=casamar5_rgbioclinic',
+
+        'casamar5_uno', 'daniel200796' );
+
     	$where='';
     	if(isset($_POST['id'])){
         		$lista=R::find( 'municipalities', ' state_id =  '.$_POST['id'],'ORDER BY name asc');
