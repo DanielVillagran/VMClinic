170e307a08c17dd82dcbaf43e83231a759f3e67f
Oswaldo <ovillagrana@ant.com.mx>
2018-10-06 11:40:07 -0500

    Siolucionando cagadero

--- assets/js/consentimiento.js
+++ assets/js/consentimiento.js
@@ -1,20 +1,38 @@
 $('#imprimirConsentimiento').click(function () {
 	var fullDate = new Date();
 	const monthNames = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"
-    ];
-    var fecha= fullDate.getDate()+" de "+monthNames[(fullDate.getMonth())]+" del "+ fullDate.getFullYear();
-    var paciente= $("#pacient_id option[value='"+$("#pacient_id").val()+"']").text();
-    var doctor=$("#medic_id option[value='"+$("#medic_id").val()+"']").text();
-    var consentimiento="Su puuta madre";
-	$.ajax({
-		url:"core/app/querys/create_letter.php",
-		type:'post',
-		data: {'fecha':fecha,'paciente':paciente,'doctor':doctor,'consentimiento':consentimiento},
-		dataType:'json',
-		success() {
+	];
+	var fecha= fullDate.getDate()+" de "+monthNames[(fullDate.getMonth())]+" del "+ fullDate.getFullYear();
+	var paciente= $("#pacient_id option[value='"+$("#pacient_id").val()+"']").text();
+	var doctor=$("#medic_id option[value='"+$("#medic_id").val()+"']").text();
 
-		}
+	var consentimiento="Su puuta madre";
+	swal({
+		type: 'info',
+		title: 'Consentimiento.',
+		text: 'A que procedimientos se sometera el paciente?',
+		input:'text',
+		showCancelButton: true,
+
+	}).then((result) => {
+		var inputValue=result.value;
+					//swal.close();
+					if (inputValue == "" || inputValue == null) {
+						swal("Error","Necesitas escribir un motivo!");
+						return false
+					}else{
+						consentimiento=inputValue;
+						$.ajax({
+							url:"core/app/querys/create_letter.php",
+							type:'post',
+							data: {'fecha':fecha,'paciente':paciente,'doctor':doctor,'consentimiento':consentimiento},
+							dataType:'json',
+							success() {
+
+							}
 
 
-	});
+						});
+					}
+				});
 });
--- core/app/querys/create_letter.php
+++ core/app/querys/create_letter.php
@@ -32,7 +32,7 @@
         }
         header('Content-Description: File Transfer');
         header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename="'.basename($filename).'"');
+        header('Content-Disposition: attachment; filename="'.basename($fileName).'"');
         header('Expires: 0');
         header('Cache-Control: must-revalidate');
         header('Pragma: public');
