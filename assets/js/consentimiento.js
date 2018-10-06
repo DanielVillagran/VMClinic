2b594c91a65117cf0b0cdd4b91f35a2c32c41274
Oswaldo <ovillagrana@ant.com.mx>
2018-10-06 10:54:25 -0500

    Siolucionando cagadero

--- assets/js/consentimiento.js
+++ assets/js/consentimiento.js
@@ -3,8 +3,8 @@ $('#imprimirConsentimiento').click(function () {
 	const monthNames = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"
     ];
     var fecha= fullDate.getDate()+" de "+monthNames[(fullDate.getMonth()+1)]+" del "+ fullDate.getFullYear();
-    var paciente= $("#pacient_id option[value="+$("#pacient_id").val()+"]").text();
-    var doctor=$("#medic_id option[value="+$("#medic_id").val()+"]").text();
+    var paciente= $("#pacient_id option:selected").text();
+    var doctor=$("#medic_id option:selected").text();
     var consentimiento="Su puuta madre";
 	$.ajax({
 		url:"core/app/querys/create_letter.php",
