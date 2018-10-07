35a37dec7d32e5e9feedfa55cffae9a8cf64a5e8
Oswaldo <ovillagrana@ant.com.mx>
2018-10-06 20:53:09 -0500

    Siolucionando cagadero

--- core/app/querys/delete_medicines.php
+++ core/app/querys/delete_medicines.php
@@ -1,6 +1,6 @@
 <?php
 require 'conexion.php';
-R::trashBatch( 'reservations_medicines', $_POST['id'] );
+R::exec( "delete from reservations_medicines WHERE id = {$_POST['id']}" );
 $lista=R::find( 'reservations_medicines', ' reservation_id =  '.$_POST['cita']);
 $lista=json_encode($lista);
 echo $lista;
